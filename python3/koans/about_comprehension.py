e753c49e1423e9ed74bbd366ab19fa5afa1d681f
happycamper1129 <happycamper.sw@gmail.com>
2013-08-21 10:22:56 -0600

    new requirments

--- python3/koans/about_comprehension.py
+++ python3/koans/about_comprehension.py
@@ -13,8 +13,8 @@ class AboutComprehension(Koan):
 
         comprehension = [delicacy.capitalize() for delicacy in feast]
 
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        #self.assertEqual(__, comprehension[0])
+        #self.assertEqual(__, comprehension[2])
 
     def test_filtering_lists_with_list_comprehensions(self):
         feast = ['spam', 'sloths', 'orangutans', 'breakfast cereals',
@@ -30,9 +30,9 @@ class AboutComprehension(Koan):
         comprehension = [ skit * number for number, skit in list_of_tuples ]
 
         self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual(__, len(comprehension[2]))
 
-    def test_double_list_comprehension(self):
+    def test_double_list_comprehention(self):
         list_of_eggs = ['poached egg', 'fried egg']
         list_of_meats = ['lite spam', 'ham spam', 'fried spam']
 
@@ -40,22 +40,21 @@ class AboutComprehension(Koan):
         comprehension = [ '{0} and {1}'.format(egg, meat) for egg in list_of_eggs for meat in list_of_meats]
 
 
-        self.assertEqual(__, comprehension[0])
         self.assertEqual(__, len(comprehension))
+        self.assertEqual(__, comprehension[0])
 
-    def test_creating_a_set_with_set_comprehension(self):
+    def test_creating_a_set_with_set_comprehention(self):
         comprehension = { x for x in 'aabbbcccc'}
 
-        self.assertEqual(__, comprehension)  # remeber that set members are unique
+        self.assertequal(__, len(comprehension))  # remeber that set members are unique
 
-    def test_creating_a_dictionary_with_dictionary_comprehension(self):
+    def test_creating_a_dictionary_with_dictionary_comprehention(self):
         dict_of_weapons = {'first': 'fear', 'second': 'surprise',
-                           'third':'ruthless efficiency', 'forth':'fanatical devotion',
-                           'fifth': None}
+                           'third':'ruthless efficiency', 'forth':'fanatical devotion', 'fifth': None}
 
-        dict_comprehension = { k.upper(): weapon for k, weapon in dict_of_weapons.items() if weapon}
+        dict_comprehention = { k.upper(): weapon for k, weapon in dict_of_weapons.iteritems() if weapon}
 
-        self.assertEqual(__, 'first' in dict_comprehension)
-        self.assertEqual(__, 'FIRST' in dict_comprehension)
+        self.assertEqual(__, 'first' in dict_comprehention)
+        self.assertEqual(__, 'FIRST' in dict_comprehention)
         self.assertEqual(__, len(dict_of_weapons))
-        self.assertEqual(__, len(dict_comprehension))
+        self.assertEqual(__, len(dict_comprehention))
